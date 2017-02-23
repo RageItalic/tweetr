a66b4c941c069524abad979a0927819305b2b836
Parth Patel <parthpatelgee@gmail.com>
2017-02-23 16:41:44 +0000

    first official commit before I switch to mongoDB. It works perfectly at this point

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,93 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//
+
+
+function createTweetElement(tweet) {
+  console.log(tweet)
+ let html = `
+ <article class="posted-tweet">
+          <header>
+            <img class="profile" src="${tweet.user.avatars.small}">
+            <h3 class="name">${tweet.user.name}</h3>
+            <h5 class="handle">${tweet.user.handle}</h5>
+          </header>
+          <div class="tweet-text">
+            <p>${escape(tweet.content.text)}</p>
+          </div>
+          <footer class="time">
+            <p>${(new Date (tweet.created_at)).toDateString()}</p>
+          </footer>
+        </article>`
+        ;
+
+  return html;
+}
+// console.log("THIS IS::" + tweet);
+
+function renderTweets(tweets) {
+  for(var tweet of tweets){
+    $(".tweet-holder").prepend(createTweetElement(tweet));
+
+  }
+  // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+}
+
+function loadTweets(){
+  $.ajax({
+    method: 'GET',
+    url: '/tweets'
+  })
+  .then(renderTweets);
+
+};
+
+$(document).ready(function($){
+  //createTweetElement(tweetData);
+  // var $tweet = createTweetElement(tweetData);
+  // renderTweets(tweetData);
+  // loadTweets();
+
+  $( "form" ).on('submit', function( event ) {
+        event.preventDefault();
+        if(!$(this).find('textarea').val()){
+          alert("At least type something first, stupid!!");
+        }else{
+        //var tweetText = text.serialize();
+        $.ajax({
+          method: 'POST',
+            url: '/tweets',
+            data: $('form').serialize(),
+        }).then(() =>{
+          //console.log("THIS SHOULD WORK.");
+          loadTweets();
+        });
+      }
+    });
+
+    $("#compose-button").click(function(){
+      $('.new-tweet').slideToggle("slow");
+      $('#hi').focus();
+    });
+
+
+  loadTweets();
+
+// Test / driver code (temporary)
+// console.log($tweet); // to see what it looks like
+//$('.tweet-holder').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+});
+
+
+
+
+
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function($){
+  updateCountdown();
+  $("textarea").change(updateCountdown);
+  $('textarea').keyup(updateCountdown);
+});
+
+//instead of doing everything inside the document ready part,
+//I did it this way because I found it on stackoverflow and
+//it made more sense to me this way becuase the syntax gets to complicated.
+//http://stackoverflow.com/questions/2136647/character-countdown-like-on-twitter
+
+function updateCountdown(){
+  var remaining = 140 - jQuery("textarea").val().length;
+  jQuery(".counter").text(remaining);
+  if(remaining < 0){
+    jQuery('span').addClass("red-text");
+    alert("WHOA! Idk what to do with all these words... try to keep them below 140");
+  }
+  else{
+    jQuery('span').removeClass("red-text");
+  }
+}
